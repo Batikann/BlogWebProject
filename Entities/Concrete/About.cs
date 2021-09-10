e9d5127f01029e1384a7185b33747d71a15859db
Batikann <emiruar123@gmail.com>
2021-09-10 19:52:55 +0300

    Add project files.

---
+++ Entities/Concrete/About.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Entities;
+
+namespace Entities.Concrete
+{
+    public class About:IEntity
+    {
+        public int AboutId { get; set; }
+        public string Details1 { get; set; }
+
+        public string Details2 { get; set; }
+
+        public string Image1 { get; set; }
+
+        public string Image2 { get; set; }
+
+        public string MapLocation { get; set; }
+    }
+}
---
+++ Entities/Concrete/Article.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Entities;
+
+namespace Entities.Concrete
+{
+    public class Article:IEntity
+    {
+        public int ArticleId { get; set; }
+        public string ArticleTitle { get; set; }
+        public string ArticleContent { get; set; }
+        public string ArticleThumbNailImage { get; set; }
+        public string ArticleImage { get; set; }
+        public DateTime ArticleCreateDate { get; set; }
+        public bool ArticleStatus { get; set; }
+    }
+}
