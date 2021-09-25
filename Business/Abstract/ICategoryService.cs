92cb7a091a08f04b43a2fd4ae27dd846095a7075
Batikann <emiruar123@gmail.com>
2021-09-25 13:37:47 +0300

    Revert "article and comment controller configuration"
    This reverts commit 2661673ca5f2a503df050c1047d25ab339db2000.

--- Business/Abstract/ICategoryService.cs
+++ Business/Abstract/ICategoryService.cs
@@ -3,17 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DataAccess.Abstract;
-using Entities.Concrete;
 
 namespace Business.Abstract
 {
     public interface ICategoryService
     {
-        void Add(Category category);
-        void Update(Category category);
-        void Delete(Category category);
-        List<Category> GetList();
-        Category GetCategory();
     }
 }
