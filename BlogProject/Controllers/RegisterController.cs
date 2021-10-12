0c5e6e4ce270d7cddd266dd0acb13e42378037cf
Batikann <emiruar123@gmail.com>
2021-10-12 13:43:09 +0300

    .Net Core Camp Homework part 2

--- BlogProject/Controllers/RegisterController.cs
+++ BlogProject/Controllers/RegisterController.cs
@@ -8,6 +8,7 @@ using Business.ValidationRules;
 using DataAccess.Concrete.EntityFramework;
 using Entities.Concrete;
 using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BlogProject.Controllers
 {
@@ -16,12 +17,13 @@ namespace BlogProject.Controllers
         private AuthorManager authorManager = new AuthorManager(new EfAuthorDal());
         public IActionResult Index()
         {
+            ViewBag.cities = GetCityList();
             return View();
         }
 
 
         [HttpPost]
-        public IActionResult Index(Author author,string passwordAgain)
+        public IActionResult Index(Author author,string passwordAgain,string cities)
         {
             AuthorValidator authorValidator = new AuthorValidator();
             ValidationResult result = authorValidator.Validate(author);
@@ -43,6 +45,26 @@ namespace BlogProject.Controllers
             
 
             return View();
+        }
+
+
+
+        public List<SelectListItem> GetCityList()
+        {
+            List<SelectListItem> adminRole=(from x in GetCity()
+                                            select new SelectListItem
+                                            {
+                                                Text = x,
+                                                Value = x
+                                            }).ToList();
+
+            return adminRole;
+        }
+
+        public List<String> GetCity()
+        {
+            String[] CitiesArray = new String[] { "Adana", "Adıyaman", "Afyon", "Ağrı", "Aksaray", "Amasya", "Ankara", "Antalya", "Ardahan", "Artvin", "Aydın", "Bartın", "Batman", "Balıkesir", "Bayburt", "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur", "Bursa", "Çanakkale", "Çankırı", "Çorum", "Denizli", "Diyarbakır", "Düzce", "Edirne", "Elazığ", "Erzincan", "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkari", "Hatay", "Iğdır", "Isparta", "İçel", "İstanbul", "İzmir", "Karabük", "Karaman", "Kars", "Kastamonu", "Kayseri", "Kırıkkale", "Kırklareli", "Kırşehir", "Kilis", "Kocaeli", "Konya", "Kütahya", "Malatya", "Manisa", "Kahramanmaraş", "Mardin", "Muğla", "Muş", "Nevşehir", "Niğde", "Ordu", "Osmaniye", "Rize", "Sakarya", "Samsun", "Siirt", "Sinop", "Sivas", "Tekirdağ", "Tokat", "Trabzon", "Tunceli", "Şanlıurfa", "Şırnak", "Uşak", "Van", "Yalova", "Yozgat", "Zonguldak" };
+            return new List<string>(CitiesArray);
         }
     }
 }
